4e0ef2c88f6ae14a32f9bdec212455d3140b8f97
Botwangsap <107746504+Botwangsap@users.noreply.github.com>
2022-07-12 11:58:54 +0700

    Update toimg2.js

--- plugins/toimg2.js
+++ plugins/toimg2.js
@@ -9,7 +9,7 @@ let handler = async (m, { conn, usedPrefix, command }) => {
   if (/webp/.test(mime)) {
     out = await webp2png(media)
   }
-  await conn.sendFile(m.chat, out, 'out.png', '*DONE*', m, false, {
+  await conn.sendFile(m.chat, out, 'out.png', 'nih kak jangan lupa follow ig ownerku kak : https://www.instagram.com/anggatzi123/', m, false, {
     thumbnail: Buffer.alloc(0)
   })
 }
